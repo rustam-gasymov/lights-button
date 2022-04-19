7a13c1bad743adb755e17930178a635f61a5ade8
rustam-cloud <gasymovrustam86@Yandex.com>
2022-04-19 12:32:25 +0300

    feat: 1111111

--- src/features/counter/counterSlice.ts
+++ src/features/counter/counterSlice.ts
@@ -3,21 +3,18 @@ import { createSlice } from "@reduxjs/toolkit";
 export const configureToolsSlice = createSlice({
   name: "configureTools",
   initialState: {
-    isLoading: false,
-    isModalVisible: false,
     value: 0,
   },
   reducers: {
-    changeVisibilityOfConfigureForm: (state, { payload }) => {
-      state.isModalVisible = payload;
-    },
     setIncrementValue: (state) => {
+      console.log("increment");
+
       state.value = state.value - 1;
     },
   },
   extraReducers: {},
 });
 
-export const { changeVisibilityOfConfigureForm, setIncrementValue } = configureToolsSlice.actions;
+export const { setIncrementValue } = configureToolsSlice.actions;
 
 export default configureToolsSlice.reducer;
