9bffa0665862ebd4779055ce7138e7b6da224f71
rustam-cloud <gasymovrustam86@Yandex.com>
2022-04-19 12:52:50 +0300

    feat: 1111111542323

--- src/components/Increment/Increment.tsx
+++ src/components/Increment/Increment.tsx
@@ -10,7 +10,7 @@ export const Increment = () => {
   console.log("dispatch");
 
   const handleClick = () => {
-    console.log("hiiiiii");
+    console.log("hiiiiii99999");
     dispatch(setIncrementValue());
   };
 
